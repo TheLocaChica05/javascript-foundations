c028a09a7dac20b4447cba90035f4d6128c99292
Brianna Brown <briannabrown@Briannas-MacBook-Air.local>
2022-06-23 21:48:07 -0400

    create unicorn class with aappropriate methods

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -2,7 +2,21 @@ class Unicorn {
   constructor(name, color) {
     this.name = name;
     this.color = color;
+    if(this.color === undefined){
+      this.color = 'white';
+    }
+  }
+  isWhite(){
+    if(this.color === 'white'){
+      return true   
+    } else {
+      return false
+    }
+  }
+  says(phrase) {
+    return `**;* ${phrase} *;**`;
   }
 }
 
 module.exports = Unicorn;
+
